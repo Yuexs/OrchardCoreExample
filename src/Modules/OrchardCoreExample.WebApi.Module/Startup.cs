db621a1400ac587c37f42b8745782ae19999094b
Yuex.S <yuex.s@xd5u.com>
2020-03-08 21:58:24 +0800

    WebApi实现Swagger

--- src/Modules/OrchardCoreExample.WebApi.Module/Startup.cs
+++ src/Modules/OrchardCoreExample.WebApi.Module/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 using OrchardCore.Security.Permissions;
+using OrchardCore.Swagger.Abstractions;
 using OrchardCoreExample.WebApi.Module.Permissions;
 
 namespace OrchardCoreExample.WebApi.Module
@@ -10,6 +11,7 @@ namespace OrchardCoreExample.WebApi.Module
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IPermissionProvider, WebApiPermission>();
+            services.AddTransient<ISwaggerApiDefinition, SwaggerApiDefinition>();
         }
     }
 }
---
+++ src/Modules/OrchardCoreExample.WebApi.Module/SwaggerApiDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+using OrchardCore.Swagger.Abstractions;
+
+namespace OrchardCoreExample.WebApi.Module
+{
+    public class SwaggerApiDefinition: ISwaggerApiDefinition
+    {
+        public string Name => "WebApiDemo";
+
+        public OpenApiInfo OpenApiInfo => new OpenApiInfo()
+        {
+            Version = "v2",
+            Title = "WebApi Demo",
+            Description = "WebApiDemo",
+            Contact = new OpenApiContact()
+            {
+                Name = "Yues.S",
+                Email = "yuex.s@xd5u.com",
+                Url = new Uri("https://www.xd5u.com")
+            }
+        };
+
+        public Func<string, ApiDescription, bool> ApiDescriptionFilterPredicate => (name, description) =>
+            description.RelativePath.StartsWith("api/demo");
+    }
+}
